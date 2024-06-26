95532400d8fbdce5669e72a75288c6cff546dc44
Narayan Bansal <narayanbansal@Narayans-MacBook-Air.local>
2024-06-26 22:33:11 +0530

    Initial commit

---
+++ ElevatorDesign/ElevatorSystem.java
@@ -0,0 +1,15 @@
+package Java.LLD.ElevatorDesign;
+
+public class ElevatorSystem {
+    private int numFloors;
+    private int maxWeight;
+    private int numElevators;
+    private static ElevatorSystem elevatorSystem = null;
+
+    private ElevatorSystem() {
+
+    }
+
+        
+    
+}   
---
+++ ElevatorDesign/Main.java
@@ -0,0 +1,7 @@
+package Java.LLD.ElevatorDesign;
+
+public class Main {
+    public static void main(String[] args) {
+        ElevatorSystem elevatorSystem = new ElevatorSystem();
+    }
+}
